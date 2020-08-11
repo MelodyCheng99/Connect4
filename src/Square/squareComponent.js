a9a42c343048616f62a3ac2fe493118d343f2d9d
Melody Cheng <melodycheng99@gmail.com>
2020-08-11 11:35:09 -0400

    Add circles instead of blue/red text - in progress

--- src/Square/squareComponent.js
+++ src/Square/squareComponent.js
@@ -3,9 +3,20 @@ import React from 'react'
 import './squareComponent.css'
 
 function Square(props) {
+    const circle = {
+        display: "inline-block",
+        marginTop: 12,
+        borderRadius: "50%",
+        width:30,
+        height:30,
+    }
+
     return (
         <button className="square" onClick={props.onClick}>
-            {props.value}
+            {props.value === 'blue' ? 
+                <div style={Object.assign({ backgroundColor: "blue" }, circle)} /> :
+                props.value === 'red' ?
+                    <div style={Object.assign({ backgroundColor: "red" }, circle)} /> : null }
         </button>
     )
 }

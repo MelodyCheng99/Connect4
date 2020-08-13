ce50dca3c0a9da5f501cc6ad4d9c01d54db94ef0
Melody Cheng <melodycheng99@gmail.com>
2020-08-13 10:58:57 -0400

    Determine if current player has won (only horizontal and vertical)

--- src/Board/boardComponent.js
+++ src/Board/boardComponent.js
@@ -9,22 +9,60 @@ class Board extends React.Component {
         super(props)
         this.state = {
             squares: Array(42).fill(null),
-            blueIsNext: true
+            blueIsNext: true,
+            winner: null,
         }
+    }
+
+    determineIfWon(curPosition, curPlayer, squares) {
+        if ((curPosition % 7) >= 3 && squares[curPosition - 1] == curPlayer &&
+            squares[curPosition - 2] == curPlayer && squares[curPosition - 3] == curPlayer) {
+            return curPlayer
+        } else if ((curPosition % 7) >= 2 && (curPosition % 7) <= 5 &&
+            squares[curPosition - 1] == curPlayer && squares[curPosition - 2] == curPlayer && squares[curPosition + 1] == curPlayer) {
+            return curPlayer
+        } else if ((curPosition % 7) >= 1 && (curPosition % 7) <= 4 &&
+            squares[curPosition - 1] == curPlayer && squares[curPosition + 1] == curPlayer && squares[curPosition + 2] == curPlayer) {
+            return curPlayer
+        } else if ((curPosition % 7) <= 3 && squares[curPosition + 1] == curPlayer &&
+            squares[curPosition + 2] == curPlayer && squares[curPosition + 3] == curPlayer) {
+            return curPlayer
+
+        } else if ((curPosition + 21) <= 41 && squares[curPosition + 7] == curPlayer &&
+            squares[curPosition + 14] == curPlayer && squares[curPosition + 21] == curPlayer) {
+            return curPlayer
+        } else if ((curPosition + 14) <= 41 && (curPosition - 7) >= 0 &&
+            squares[curPosition + 7] == curPlayer && squares[curPosition + 14] == curPlayer && squares[curPosition - 7] == curPlayer) {
+            return curPlayer
+        } else if ((curPosition + 7) <= 41 && (curPosition - 14) >= 0 &&
+            squares[curPosition + 7] == curPlayer && squares[curPosition - 7] == curPlayer && squares[curPosition - 14] == curPlayer) {
+            return curPlayer
+        } else if ((curPosition - 21) >= 0 && squares[curPosition - 7] == curPlayer &&
+            squares[curPosition - 14] == curPlayer && squares[curPosition - 21] == curPlayer) {
+            return curPlayer
+        }
+
+        return null
     }
 
     selectSquare(i) {
         const squares = this.state.squares.slice()
+        let winner = this.state.winner
+
         for (let j = 5; j > 0; j--) {
             const curPosition = (7 * j) + (i % 7)
             if (squares[curPosition] == null) {
-                squares[curPosition] = this.state.blueIsNext ? 'blue' : 'red'
+                const curPlayer = this.state.blueIsNext ? 'blue' : 'red'
+                squares[curPosition] = curPlayer
+                winner = this.determineIfWon(curPosition, curPlayer, squares)
                 break
             }
         }
+
         this.setState({
             squares: squares,
-            blueIsNext: !this.state.blueIsNext
+            blueIsNext: !this.state.blueIsNext,
+            winner: winner
         })
     }
 
@@ -52,13 +90,17 @@ class Board extends React.Component {
 
     render() {
         return (
-            <div className="board">
-                {this.renderRow(0)}
-                {this.renderRow(1)}
-                {this.renderRow(2)}
-                {this.renderRow(3)}
-                {this.renderRow(4)}
-                {this.renderRow(5)}
+            <div>
+                { this.state.winner ? <span> You Win! </span> : null }
+
+                <div className="board">
+                    {this.renderRow(0)}
+                    {this.renderRow(1)}
+                    {this.renderRow(2)}
+                    {this.renderRow(3)}
+                    {this.renderRow(4)}
+                    {this.renderRow(5)}
+                </div>
             </div>
         )
     }
